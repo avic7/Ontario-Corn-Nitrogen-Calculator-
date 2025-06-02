66cc1ff49f06941da871c2ebc77989095d81fa7b
Atharva Vichare <148368569+avic7@users.noreply.github.com>
2025-06-01 22:58:54 -0400

    Update server.R

--- server.R
+++ server.R
@@ -3,23 +3,29 @@ server <- function(input, output, session) {
   soil_eo <- readxl::read_excel("SoilEO.xlsx")
   soil_wo <- readxl::read_excel("SoilWO.xlsx")
   crop_adj <- readxl::read_excel("PreviousCrop.xlsx")
+  fertilizer_data <- readxl::read_excel("Fertilizers.xlsx")
   
+  # Soil Selection Reactive
   selected_soil <- reactive({
     if (input$region == "Eastern Ontario") {
-      soil_eo %>% filter(tolower(trimws(`Soil Type`)) == tolower(trimws(input$soil_type)))
+      soil_eo %>% filter(tolower(trimws(.data[["Soil Type"]])) == tolower(trimws(input$soil_type)))
     } else if (input$region == "Western Ontario") {
-      soil_wo %>% filter(tolower(trimws(`Soil Type`)) == tolower(trimws(input$soil_type)))
+      soil_wo %>% filter(tolower(trimws(.data[["Soil Type"]])) == tolower(trimws(input$soil_type)))
     }
   })
   
-  # Yield Adjustment Calculations
+  crop_adjustment <- reactive({
+    crop_adj %>% filter(.data[["Previous Crop"]] == input$previous_crop)
+  })
+  
+  # Yield Adjustment Calculation
   target_yield <- reactive({
     yield_imperial <- input$yield_adjustment * 0.77
     yield_metric <- yield_imperial * 1.12
     list(imperial = yield_imperial, metric = yield_metric)
   })
   
-  # Heat Unit Adjustment Calculations
+  # Heat Unit Adjustment Calculation
   heat_unit_adj <- reactive({
     chu <- input$heat_units
     hu_imperial <- (chu - 2800) * 0.036
@@ -111,21 +117,38 @@ server <- function(input, output, session) {
         div(class = "icon", icon("leaf"))
     )
   })
-
   
-  # nitrogen price
+  # N content for selected fertilizer
+  get_n_content <- reactive({
+    req(input$fertilizer_product)
+    selected_fertilizer <- fertilizer_data %>% 
+      filter(`Fertilizer Type` == input$fertilizer_product)
+    
+    if(nrow(selected_fertilizer) > 0) {
+      return(selected_fertilizer$`kg N/tonne`[1])
+    } else {
+      return(0)
+    }
+  })
+  
+  # Nitrogen price calculation
   output$nitrogen_price_output <- renderText({
-    if (!is.null(input$fertilizer_price_tonne) && !is.null(input$fertilizer_product)) {
-      # Conversion factors for different fertilizers (% N content)
-      n_content <- switch(input$fertilizer_product,
-                          "Urea" = 0.46,
-                          "Anhydrous Ammonia" = 0.82,
-                          "UAN Solution" = 0.28)
+    req(input$fertilizer_price_tonne, input$fertilizer_product, get_n_content())
+    
+    n_content_kg_per_tonne <- get_n_content()
+    
+    if(n_content_kg_per_tonne > 0) {
+      # Converting price per tonne to price per kg of product
+      price_per_kg_product <- input$fertilizer_price_tonne / 1000
       
+      # Converting kg N/tonne to fraction
+      n_fraction <- n_content_kg_per_tonne / 1000
       
-      # Converting tonne to lbs and calculate price per lb of actual N
-      price_per_lb_product <- input$fertilizer_price_tonne / 2204.62  # tonne to lbs
-      price_per_lb_n <- price_per_lb_product / n_content
+      # Calculating price per kg of actual N
+      price_per_kg_n <- price_per_kg_product / n_fraction
+      
+      # Converting to price per lb of actual N
+      price_per_lb_n <- price_per_kg_n / 2.205
       
       paste0("$", round(price_per_lb_n, 2))
     } else {
@@ -133,69 +156,70 @@ server <- function(input, output, session) {
     }
   })
   
+  # Net corn price calculation
   output$net_corn_price_output <- renderText({
-    if (!is.null(input$corn_price)) {
-      if (input$region == "Eastern Ontario") { 
-        
-        # For Eastern Ontario, subtracting drying charges and transportation/marketing
-        drying <- ifelse(is.null(input$drying_charges), 0, input$drying_charges)
-        transport <- ifelse(is.null(input$transport_marketing), 0, input$transport_marketing)
-        net_price <- input$corn_price - drying - transport
-      } else {
-        
-        # For Western Ontario, using corn price as it is
-        net_price <- input$corn_price
-      }
-      paste0("$", round(net_price, 2))
+    req(input$corn_price)
+    
+    if (input$region == "Eastern Ontario") { 
+      # Eastern Ontario
+      drying <- ifelse(is.null(input$drying_charges), 0, input$drying_charges)
+      transport <- ifelse(is.null(input$transport_marketing), 0, input$transport_marketing)
+      net_price <- input$corn_price - drying - transport
     } else {
-      "$0.00"
+      # Western Ontario
+      net_price <- input$corn_price
     }
+    paste0("$", round(net_price, 2))
   })
   
-  
+  # Reactive expression for net corn price
   net_corn_price <- reactive({
-    if (!is.null(input$corn_price)) {
-      if (input$region == "Eastern Ontario") { 
-        drying <- ifelse(is.null(input$drying_charges), 0, input$drying_charges)
-        transport <- ifelse(is.null(input$transport_marketing), 0, input$transport_marketing)
-        return(input$corn_price - drying - transport)
-      } else {
-        return(input$corn_price)
-      }
+    req(input$corn_price)
+    
+    if (input$region == "Eastern Ontario") { 
+      drying <- ifelse(is.null(input$drying_charges), 0, input$drying_charges)
+      transport <- ifelse(is.null(input$transport_marketing), 0, input$transport_marketing)
+      return(input$corn_price - drying - transport)
+    } else {
+      return(input$corn_price)
     }
-    return(0)
   })
   
-  # Update price ratio calculation to use the reactive expression:
+  # Reactive expression for nitrogen price per lb
+  nitrogen_price_per_lb <- reactive({
+    req(input$fertilizer_price_tonne, input$fertilizer_product, get_n_content())
+    
+    n_content_kg_per_tonne <- get_n_content()
+    
+    if(n_content_kg_per_tonne > 0) {
+      # Converting price per tonne to price per kg of product
+      price_per_kg_product <- input$fertilizer_price_tonne / 1000
+      
+      # Convertoing kg N/tonne to fraction
+      n_fraction <- n_content_kg_per_tonne / 1000
+      
+      # Calculating price per kg of actual N
+      price_per_kg_n <- price_per_kg_product / n_fraction
+      
+      # Converting to price per lb of actual N 
+      price_per_lb_n <- price_per_kg_n / 2.205
+      
+      return(price_per_lb_n)
+    } else {
+      return(0)
+    }
+  })
+  
+  # Price ratio calculation
   output$price_ratio_output <- renderText({
-    if (!is.null(input$fertilizer_price_tonne) && !is.null(input$fertilizer_product) && net_corn_price() > 0) {
-      n_content <- switch(input$fertilizer_product,
-                          "Urea" = 0.46,
-                          "Anhydrous Ammonia" = 0.82,
-                          "UAN Solution" = 0.28)
-      
-      price_per_lb_product <- input$fertilizer_price_tonne / 2204.62
-      price_per_lb_n <- price_per_lb_product / n_content
-      
-      
-      
-      # Calculate price ratio
-      price_ratio <- price_per_lb_n / net_corn_price()
-      
+    req(nitrogen_price_per_lb(), net_corn_price())
+    
+    if(net_corn_price() > 0 && nitrogen_price_per_lb() > 0) {
+      price_ratio <- nitrogen_price_per_lb() / net_corn_price()
       round(price_ratio, 1)
     } else {
       "0.0"
     }
-  })
-  
-  # Calculate lb/ac adjustment
-  output$adjustment_lb_ac_output <- renderText({
-    "-38"  # Placeholder 
-  })
-  
-  # Calculate kg/ha adjustment
-  output$adjustment_kg_ha_output <- renderText({
-    "-42"  # Placeholder 
   })
   
   # Total Nitrogen Recommendation (Imperial)
@@ -252,4 +276,46 @@ server <- function(input, output, session) {
     req(input$manure_credit_imperial)
     paste0(round(input$manure_credit_imperial * 1.13, 1), " kg/ha")
   })
+  
+  # Price Ratio Adjustment Calculation
+  price_ratio <- reactive({
+    req(nitrogen_price_per_lb(), net_corn_price())
+    
+    # C19/56
+    corn_price_per_lb <- net_corn_price() / 56  
+    ratio <- nitrogen_price_per_lb() / corn_price_per_lb
+    
+    return(ratio)
+  })
+  
+  # Reactive Price Ratio Adjustment
+  price_ratio_adjustment <- reactive({
+    req(price_ratio())
+    
+    ratio <- price_ratio()
+    
+    # (C21-5)*6 (C21-5)*6.27
+    imperial_adj <- -(ratio - 5) * 6
+    metric_adj <- -(ratio - 5) * 6.7
+    
+    list(imperial = imperial_adj, metric = metric_adj)
+  })
+  
+  # Price Ratio Adjustment outputs
+  output$price_ratio_value <- renderText({
+    req(price_ratio())
+    round(price_ratio(), 2)  
+  })
+  
+  output$price_ratio_imperial <- renderText({
+    req(price_ratio_adjustment())
+    paste0(round(price_ratio_adjustment()$imperial, 1), " lb/ac")
+  })
+  
+  output$price_ratio_metric <- renderText({
+    req(price_ratio_adjustment())
+    paste0(round(price_ratio_adjustment()$metric, 1), " kg/ha")
+  })
+ 
 }
+  
